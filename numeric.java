240ec79159a85944ce7037ea381d5d795fed9336
sindhu555 <41734778+sindhu555@users.noreply.github.com>
2018-08-01 15:03:26 +0530

    Update and rename line.java to numeric.java

---
+++ numeric.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+			     Scanner sc=new Scanner(System.in);
+               String str=sc.nextLine();
+               int count=0;
+               for(int i=1;i<str.length();i++)
+               {
+               	if(str.charAt(i)=='1' || str.charAt(i)=='2' || str.charAt(i)=='3'|| str.charAt(i)=='4' || str.charAt(i)=='5' || str.charAt(i)=='6' || str.charAt(i)=='7' || str.charAt(i)=='8' || str.charAt(i)=='9' || str.charAt(i)=='0')
+               	{
+               		count++;
+               	}
+               }
+               System.out.println(count);
+	}
+}	
+	
