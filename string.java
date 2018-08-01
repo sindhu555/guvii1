59e5ed79365f3188d1a3fddc545aedd0e79c8c03
sindhu555 <41734778+sindhu555@users.noreply.github.com>
2018-08-01 13:38:43 +0530

    Update and rename README.md to string.java

---
+++ string.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		String str=sc.nextLine();
+		if(isNumeric(str))
+			System.out.println("Yes");
+		else
+			System.out.println("No");
+	}
+	public static boolean isNumeric(String str)
+	{
+		return str.matches("-?\\d+(\\.\\d+)?");
+	}
+}
